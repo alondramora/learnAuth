ac6da0d34b946141bdb87d6ea8ce386f419c7cca
alondramora <mora_ac@yahoo.com>
2023-02-11 21:20:54 -0800

    created index, login, and register routes

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+//Routes
+app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 5200;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
